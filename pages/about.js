2b52a013a10d976bbb68b7be10f7e804a0ad2970
Atsushi Kayama <a.shika829@gmail.com>
2020-08-16 13:39:09 +0900

    init repo

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Head from 'next/head'
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        About page
+      </div>
+    )
+  }
+}
+
+export default About
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Head from 'next/head'
+
+class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        index page
+      </div>
+    )
+  }
+}
+
+export default Index
